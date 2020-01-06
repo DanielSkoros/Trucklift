b4f167321ac7a288a1d8f20fd545fa00de36a914
DanielSkoros <morsmorfie@gmail.com>
2020-01-06 16:50:53 +0100

    init

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+const trigger = document.querySelector('.navTrigger');
+
+const menuContainer = document.querySelector('.nav__container');
+
+const mainContainer = document.querySelector('main');
+
+const fixedLogo = document.querySelector('.mobile__fixed');
+
+trigger.addEventListener('click', () => {
+   mainContainer.classList.toggle('hide');
+   fixedLogo.classList.toggle('hide');
+   menuContainer.classList.toggle('visible');
+   trigger.classList.toggle('active');
+});
